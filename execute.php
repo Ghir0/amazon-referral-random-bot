5a196368c493b7ff1af4ba5fa567963b83de3a5d
Mike Tamanti <mike.tamanti@gmail.com>
2016-11-13 00:19:42 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -43,7 +43,7 @@ $dominio = $array1[1];
 	//$response = "Good! This is an ".$dominio." link!!";
 	$url_to_parse = $message['text'];
 	$url_affiliate = set_referral_URL($url_to_parse);
-	$response = $url_affiliate." ASIN:".$ASIN." key_ASIN:".$key_ASIN." path:".var_dump($path)." parsed_url:".var_dump($parsed_url_array);
+	$response = $url_affiliate." ASIN:".$ASIN." key_ASIN:".$key_ASIN." path:".print_r($path)." parsed_url:".print_r($parsed_url_array);
 	
   }
   elseif(strcmp($array1[0],"www") === 0)
