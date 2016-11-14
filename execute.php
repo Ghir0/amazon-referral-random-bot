edc5e8e74071b6a3faf51745cac4679a941135ee
Mike Tamanti <mike.tamanti@gmail.com>
2016-11-14 18:13:18 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -74,9 +74,8 @@ function set_referral_URL($url){
 }
 
 function clean_for_URL($string){
-	$cleaned_string = "";
 	$cleaned_string = explode(' ',strstr($string,'https://'))[0];
-	if($cleaned_string == ""){ explode(' ',strstr($string,'http://'))[0]; }
+	if(strcmp($cleaned_string,"false") === 0){ $cleaned_string = explode(' ',strstr($string,'http://'))[0]; }
 	return $cleaned_string;
 }
 /*
