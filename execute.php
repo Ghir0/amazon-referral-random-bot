92e80a4b5caa65832c6ae9c6b553844f1fc2b529
Mike Tamanti <mike.tamanti@gmail.com>
2016-11-14 12:33:02 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -43,7 +43,7 @@ if(isset($message['text']))
 	$url_affiliate = set_referral_URL($url_to_parse);
 	$faccinasym = json_decode('"\uD83D\uDE00"');
 	$linksym =  json_decode('"\uD83D\uDD17"');
-	$response = "Ecco fatto! Di seguito il link che hai richiesto $faccinasym \n$linksym:  $url_affiliate";
+	$response = "Ecco fatto! Di seguito il link che hai richiesto $faccinasym \n$linksym  $url_affiliate";
   }
   elseif(strcmp($array1[0],"www") === 0)
   {
